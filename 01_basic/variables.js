95773eed2763fd17427dfd149e1b2b54f1db7e71
Dev162-creator <ankitgupta143mail.com@gmail.com>
2024-12-06 21:42:34 +0000

    happy

---
+++ 01_basic/variables.js
@@ -0,0 +1,13 @@
+const accountId =1466574
+let accountEmail = "ankitgupta143mail.com@gmail.com"
+var accountPassword = "543645"
+accountcity = "kasganj" // that is var type value please do not use this is not a good way to write code
+// not allowed
+//accountId =146
+accountEmail="devanshu@gmail"
+console.log(accountEmail);
+console.log(accountId);
+console.log([accountEmail,accountId]);
+// if you suggest do not use var because it have essue to block scop or functional scope.
+let accountState; // if you not provide value that is undefined .
+console.log(accountState);
