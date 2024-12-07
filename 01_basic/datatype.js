0a078ffe769dcbd585ee78a782402eb56ad4d5a6
Dev162-creator <ankitgupta143mail.com@gmail.com>
2024-12-07 21:24:03 +0000

    hello

---
+++ 01_basic/datatype.js
@@ -0,0 +1,19 @@
+ let name = "hitesh"
+ let age =18
+ //let is loggedIn = false
+
+ // number => 2 to power 53
+ // bigint
+ // string => ""
+ // boolean => true/false
+ // null => standalone value
+ // undefined =>
+//  symbol => unique
+
+ // object 
+ 
+
+
+ console.log(typeof age);
+ console.log(typeof name);
+ 
