e53940c85a6514930afa5de22d6a1e7b6adcbdb3
Dev162-creator <ankitgupta143mail.com@gmail.com>
2024-12-08 14:44:28 +0000

    jello

---
+++ 01_basic/datatypesummary.js
@@ -0,0 +1,25 @@
+// primitive
+//=> 7 types : String,number,Boolean,null ,undefined, symbol,BigInt
+
+//reference type also called  non primitive
+
+// array,objects, functions 
+
+const score =100
+const scoreValue = 100.3 
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+
+
+const id= Symbol('2345')
+const anotherId= Symbol('2345')
+console.log(id===anotherId);
+
+
+
+
+
+
+
+
