ce77355275b9ce97ea90ccf32aa7176eb7df6463
Dev162-creator <ankitgupta143mail.com@gmail.com>
2024-12-08 11:29:57 +0000

    new

---
+++ 01_basic/conversion.js
@@ -0,0 +1,43 @@
+/*let score ="33sjdhs"
+console.log(typeof score)    // type one
+console.log(typeof (score))    //  type 2
+
+let valueIn// if we have undefined value thanNumber = Number(score)
+console.log(typeof valueInNumber);  // number means nan 
+console.log( valueInNumber); // give real value type */
+
+/*let score = null   // give that null type 0
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); 
+console.log( valueInNumber)*/
+
+
+// if the value undefined than
+
+/*let score = undefined  // that is give nan type
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); 
+console.log( valueInNumber)*/
+
+
+// if the value boolean than
+
+/*let score = true  // that is give true 1 or false 0 type
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); 
+console.log( valueInNumber)*/
+
+
+// nan =>not an number
+/*let isLoggedIn= "devanshu"
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//console.log(typeof booleanIsLoggedIn);*/
+/*
+1 => true
+false =>0
+"devanshu"=> true*/
+
+
+
+
